16d9ec995a33f6ebbcc77be80a722abb6b31e146
Ian Jonas Hiroce <hirocex@gmail.com>
2023-05-08 15:18:01 -0300

    adicionado exercicios ForEach

--- Secao-7/HOFS-ForEach-Find-Some-Every/ForEach/forEach.js
+++ Secao-7/HOFS-ForEach-Find-Some-Every/ForEach/forEach.js
@@ -1,11 +1,12 @@
-const emailListInData = [
-  'roberta@email.com',
-  'paulo@email.com',
-  'anaroberta@email.com',
-  'fabiano@email.com',
-];
+// const emailListInData = [
+//   'roberta@email.com',
+//   'paulo@email.com',
+//   'anaroberta@email.com',
+//   'fabiano@email.com',
+// ];
 
-// Adicione seu código aqui
+// emailListInData.forEach((email) => console.log(`O email ${email} está cadastrado em nosso banco de dados!`));
 
-emailListInData.forEach((element) => {
-})
+const Jogadores =['Lionel Messi', 'Cristiano Ronaldo', 'Neymar Junior', 'Kyllian Mbappe', 'Luis Suarez'];
+
+Jogadores.forEach((jogador) => console.log(`O jogador ${jogador} é muito bom!!!!!!`));
